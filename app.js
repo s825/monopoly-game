da43f3b75c63c37af78c1811b12977b472f2a280
LESLIE SERENITY <150037681@cart.ads>
2020-01-15 09:46:58 -0800

    new code and fixed spaces

--- app.js
+++ app.js
@@ -14,17 +14,52 @@ function printNumber(num1, num2) {
 
 var button = document.getElementById('button');
 
-button.onclick = function() {
-  var result1 = dice.roll();
-  var result2 = dice.roll();
-  printNumber(result1,result2);
-};
+//button.onclick = takeTurn();
+
+
+
+//to play game:
+function takeTurn(){
+  rollDice();
+  movePiece(player,rolled);
+}
+
+
+
+//set players turn
+let players = [1,2,3,4],
+    player = 1;
+
+function changePlayer(curPlayer){
+  if(curPlayer < players.length){
+    player++
+  } else {
+    player = 1;
+  }
+}
+
+
+
+//roll dice
+let rolled = 0;
+
+function rollDice(){
+    let result1 = dice.roll(),
+      result2 = dice.roll(),
+      total = result1 + result2;
+    printNumber(result1,result2);
+    rolled = total;
+}
+
+//move by dice roll
 function movePiece(piece, moveBy) {
-  currentSpace = ?;
+  currentSpace = 2;
   targetSpace = currentSpace + moveBy;
   piece = document.getElementById(piece);
   piece.remove()
   targetSpace = document.querySelector(`#space${targetSpace} .container`);
   targetSpace.appendChild(piece);
+} //figure out how to get your current piece location and modify thos function so it moves.
 
-}
+
+
